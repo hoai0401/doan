bdeaa686fdde0c80315692b7b3300e3fcdc50ba6
hoai0401 <tranhoai1564@gmail.com>
2024-01-03 16:17:44 +0700

    chuyeenr js

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="{{ asset('fonts/themify-icons/themify-icons.css') }}">
     <link rel="stylesheet" href="{{ asset('css/logo.css') }}">
     <link rel="stylesheet" href="//cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
+    <script src="{{ asset('js/js.js') }}" defer></script>
 
 </head>
 <body>
@@ -208,50 +209,5 @@
 
 
 </body>
-<script src="//cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-<script>
-    const imgPosition = document.querySelectorAll(".slider-container img")
-    const imgContainer = document.querySelector(".slider-container")
-    const dotItem = document.querySelectorAll(".dot")
-    let imgSlider = imgPosition.length
-    let index = 0
-    // console.log(imgPosition)
-    imgPosition.forEach(function(image, index){
-        image.style.left = index*100 + "%"
-        dotItem[index].addEventListener("click", function(){
-            slider(index)
-        })
-    })
-    function imgSlide(){
-        index++;
-        if(index >= imgSlider){
-            index = 0
-        }
-        slider(index)
 
-    }
-
-    function slider(index){
-        imgContainer.style.left = "-" +index*100+ "%"
-        const dotActive = document.querySelector(".active")
-        dotActive.classList.remove("active")
-        dotItem[index].classList.add("active")
-    }
-
-     document.querySelector('.user-panel').addEventListener('mouseover', function () {
-        // Show the dropdown content
-        document.querySelector('.dropdown-content').style.display = 'block';
-        document.getElementById('user-name').classList.add('active');
-    });
-
-    document.querySelector('.user-panel').addEventListener('mouseout', function () {
-        // Hide the dropdown content
-        document.querySelector('.dropdown-content').style.display = 'none';
-        document.getElementById('user-name').classList.remove('active');
-    });
-
-</script>
-<script>$(document).ready( function () {
-    $('#myTable').DataTable();
-} );</script>
 </html>
